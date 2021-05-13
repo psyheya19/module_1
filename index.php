823494a6c044da967c11a6af8b57c2280a61056a
Катюша <psyheya19@mail.ru>
2021-05-13 12:46:32 +0300

    В приложение добавлено возвращение корректного по структуре json для поиска авторов, книг, неподдерживаемых запросов

--- index.php
+++ index.php
@@ -1,8 +1,39 @@
 <?php
 if ('/books'===$_SERVER['PATH_INFO']) {
-    echo 'books';
+    $httpCode = 200;
+    $result = [
+        [
+            'id' => 1,
+            'title' => 'Бойцовский клуб',
+            'year' => '1996',
+            'author' => [
+                'id'=> 1,
+                'name' => 'Чак',
+                'surname' => 'Паланик',
+                'birthday' => '21.02.1962',
+                'country' => 'us',
+            ],
+        ]
+    ];
 } elseif ('/authors'===$_SERVER['PATH_INFO']) {
-    echo 'authors';
+    $httpCode = 200;
+    $result = [
+       [
+           'id'=> 1,
+           'name' => 'Чак',
+           'surname' => 'Паланик',
+           'birthday' => '21.02.1962',
+           'country' => 'us',
+       ]
+    ];
+
 } else {
-    echo 'неизвестный текст';
-}
+    $httpCode = 404;
+    $result = [
+        'status' => 'fail',
+        'message' => 'unsupported request'
+    ];
+   }
+
+http_response_code($httpCode);
+echo json_encode($result);
